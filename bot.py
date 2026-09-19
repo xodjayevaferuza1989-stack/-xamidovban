d90c554e06f07bd4d22ada84260956271c52ef07
xodjayevaferuza1989-stack <xodjayevaferuza1989@gmail.com>
2026-09-19 17:24:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, LabeledPrice
 
 # --- SOZLAMALAR ---
-TOKEN = "8711985924:AAEROk3VMvDe6fVRS1TmPRMq0fyjqfmGLwI"
+TOKEN = "8711985924:AAE4Rtj_6lodJhCGF0Jhzj473VnG14-0sb8"
 ADMIN_ID = 8587976365
 # ------------------
 
